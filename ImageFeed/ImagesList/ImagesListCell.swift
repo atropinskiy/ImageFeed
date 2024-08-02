101be9e4fe944e5ab87f3092798ef3a229a4440a
atropinskiy <vaskaop@mail.ru>
2024-08-02 21:58:04 +0300

    Спринт 8. Выполнен

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,16 @@
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var cellImage: UIImageView!
+
+    
+    
+    
+
+}
